fade199f0a9197b237866e3d241f72cbd85dcfdc
Federico Terraneo <f.terraneo@hotmail.com>
2025-07-07 17:36:33 +0200

    cleanup, basic home page with links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config"
-import preact from "@astrojs/preact"
 
 export default defineConfig({
     site: "https://mirrorball.it",
-    integrations: [preact()],
 })
